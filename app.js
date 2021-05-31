fdcc27bf16ffc035521cd6a68a96561791ce5f2f
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2021-06-01 00:11:37 +0300

    Project files have been created.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req,res) => {
+
+    res.send('INDEX SAYFASI')
+
+})
+
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`App started on port ${port}`)
+})
