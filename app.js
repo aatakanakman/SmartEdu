c79f0f4b84064b45cbd1fb0f7ce5eb760f8f63fb
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2021-06-01 00:32:57 +0300

    partials has been created

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+
+app.set('view engine','ejs');
+
+//Middlewares
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
 
-    res.send('INDEX SAYFASI')
+    res.status(200).render('index',{
+        page_name : 'index',
+    })
 
 })
+
+app.get('/about', (req,res) => {
+
+    res.status(200).render('about',{
+        page_name : 'about',
+    })
+
+})
+
 
 const port = 3000;
 app.listen(port,()=>{
